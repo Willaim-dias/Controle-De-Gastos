b46f97fa37d5e5d23e0699825e49744f722f91d2
Willaim-dias <williampacifico41@gmail.com>
2025-01-14 22:04:04 -0300

    first commit

---
+++ src/view/util/Utils.java
@@ -0,0 +1,152 @@
+package view.util;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.stage.Stage;
+import javafx.util.StringConverter;
+
+public class Utils {
+
+    public static Stage currentStage(ActionEvent event) {
+        return (Stage) ((Node) event.getSource()).getScene().getWindow();
+    }
+
+    public static Integer tryParseToInt(String str) {
+        try {
+            return Integer.valueOf(str);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    public static Double tryParseToDouble(String str) {
+        try {
+            return Double.valueOf(str);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+    
+    public static Double formatNumber(String str) {
+        try {
+            String format = str.replaceAll("\\.","").replaceAll(",", ".");
+            return Double.valueOf(format);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+    
+    public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
+        tableColumn.setCellFactory(column -> {
+            TableCell<T, Date> cell = new TableCell<T, Date>() {
+                private SimpleDateFormat sdf = new SimpleDateFormat(format);
+
+                @Override
+                protected void updateItem(Date item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setText(null);
+                    } else {
+                        setText(sdf.format(item));
+                    }
+                }
+            };
+
+            return cell;
+        });
+    }
+
+    public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
+        tableColumn.setCellFactory(column -> {
+            TableCell<T, Double> cell = new TableCell<T, Double>() {
+
+                @Override
+                protected void updateItem(Double item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setText(null);
+                    } else {
+                        Locale.setDefault(Locale.US);
+                        setText(String.format("%." + decimalPlaces + "f", item));
+                    }
+                }
+            };
+
+            return cell;
+        });
+    }
+
+    public static void formatDatePicker(DatePicker datePicker, String format) {
+        datePicker.setConverter(new StringConverter<LocalDate>() {
+
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(format);
+
+            {
+                datePicker.setPromptText(format.toLowerCase());
+            }
+
+            @Override
+            public String toString(LocalDate date) {
+                if (date != null) {
+                    return dateFormatter.format(date);
+                } else {
+                    return "";
+                }
+            }
+
+            @Override
+            public LocalDate fromString(String string) {
+                if (string != null && !string.isEmpty()) {
+                    return LocalDate.parse(string, dateFormatter);
+                } else {
+                    return null;
+                }
+            }
+        });
+    }
+
+    public static boolean validation(String passwordUser, String passowordDb) {
+        return passwordUser.equals(passowordDb);
+    }
+    
+    public static String codeUser(String userName) {
+        String initials = userName.substring(0, 2).toUpperCase();
+        Random random = new Random();
+        int number = 1000 + random.nextInt(9000);
+        return initials + number;
+    }
+    
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedHash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+
+            StringBuilder hexHash = new StringBuilder();
+
+            for (byte b : encodedHash) {
+
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexHash.append('0');
+                }
+                hexHash.append(hex);
+            }
+            return hexHash.toString();
+        } catch (NoSuchAlgorithmException e) {
+        }
+        return "";
+    }
+    
+}
